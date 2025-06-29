702afc03781bee59975e64ff9b861611bcb3ca27
gunshipac130 <chawis_49@hotmail.com>
2025-06-30 06:18:42 +0700

    cleanup

--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
-1. paste page source to '__test.txt__'
+1. press __F12__ in brower, copy __'html'__ element then paste into '__test.txt__'
+##Do not use __'View page source'__
 2. delete text '__<!DOCTYPE html>__' and any whitespace at start of text file
 3. run '__twitch-regex.py__'
 4. transplant list in '__Unwanted_list.txt__' to '__UnwantedTwitch_Blacklist.json__'
--- twitch-regex.py
+++ twitch-regex.py
@@ -5,35 +5,20 @@ ch = []
 
 textfile = open('test.txt', 'r', encoding='utf8')
 file_contents = textfile
-#print(file_contents)
+# print(file_contents) #check txt
 
 print('check is string number only\n----------------------------------------')
 for line in file_contents:
     tempstring = re.findall('alt="(\S+)"\ssrc', line)
-    #print(tempstring)
+    # print(tempstring) #check regex output
 
 for _ch in tempstring:
     print(_ch.ljust(25), not _ch.isdigit())
     if not _ch.isdigit():
         ch.append(_ch)
-        #print(ch)
-
-#print('----------------------------------------')
-#print(ch)
-#print('----------------------------------------')
-#for x in ch:
-#    print(x)
+        # print(ch) #check is number(not channal?) output
 
 textfile.close()
-
-
-
-
-#with open("file.json", "w") as f:
-#    json.dump([{"f_name": f_name, "l_name": l_name}], f)
-
-
-
 
 toLongText = ""
 for _ch in tempstring:
@@ -41,17 +26,9 @@ for _ch in tempstring:
         toLongText = f'"{_ch}"'
     else:
         toLongText = f'{toLongText},"{_ch}"'
-
 toLongText = (f'[{toLongText}]')
 
 with open("Unwanted_list.txt", "w", encoding="utf-8") as outfile:
     outfile.write(toLongText)
 
-
-
-
-
-
-#input('----------------------------------------\nPress Enter to continue...')
-
-#alt="(\S+)"\ssrc
+input('----------------------------------------\nPress Enter to continue...')
